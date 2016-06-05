db104940df5fff03950ac9567ffd0cef1ada0161
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-05 13:11:21 -0300

    Factorizador indica que un numero primo es primo.

--- factores-primos-3/model/factorizador.rb
+++ factores-primos-3/model/factorizador.rb
@@ -1,11 +1,19 @@
 class Factorizador	
 
+	attr_accessor :numero_a_factorizar
+
 	def initialize numero
 		@numero_a_factorizar = numero
 	end
 
-	def numero_a_factorizar
-		@numero_a_factorizar
+	def numero_es_primo
+		es_primo = true
+		for numero in 2...numero_a_factorizar
+			if numero_a_factorizar % numero == 0
+				es_primo = false
+			end
+		end
+		es_primo
 	end
 
 end
--- factores-primos-3/spec/factorizador_spec.rb
+++ factores-primos-3/spec/factorizador_spec.rb
@@ -7,5 +7,10 @@ describe Factorizador do
 	  factorizador = Factorizador.new 5
     expect(factorizador.numero_a_factorizar).to eq 5
   end
+
+  it 'deberia indicar que un numero primo es primo' do
+  	factorizador = Factorizador.new 5
+  	expect(factorizador.numero_es_primo).to eq true
+  end
 	
 end
