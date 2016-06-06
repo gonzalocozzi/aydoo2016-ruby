b76e4dc08639722143b1213b5f8559b1af2f45ca
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-05 23:15:37 -0300

    Escritor de archivo indica la direccion completa del archivo escrito con la factorizacion.

--- factores-primos-3/model/escritor_de_archivo.rb
+++ factores-primos-3/model/escritor_de_archivo.rb
@@ -8,15 +8,23 @@ class EscritorDeArchivo
     @argumento_de_direccion = argumento_de_direccion
   end
 
+  def obtener_direccion_de_archivo nombre_de_archivo
+  	direccion_absoluta_de_archivo = File.dirname(__FILE__)
+	direccion_de_carpeta_madre = Pathname.new(direccion_absoluta_de_archivo).parent.to_s
+	direccion_de_carpeta_madre + "/" + nombre_de_archivo
+  end
+
   def escribir_archivo
-
   	if argumento_de_direccion.length == 14
-      "No ha especificado un nombre de archivo. Por favor, intente nuevamente."
+      "No ha especificado el nombre del archivo a escribir. Por favor, intente nuevamente."
   	else
-  	nombre_de_archivo = argumento_de_direccion[14..argumento_de_direccion.length - 1]
-  	  "El resultado de la factorizacion ha sido escrito con exito en " + nombre_de_archivo 
+  	  nombre_de_archivo = argumento_de_direccion[14..argumento_de_direccion.length - 1]  	  
+  	  f = File.open(nombre_de_archivo, 'w')
+	  f.puts salida	  
+	  f.close
+	  direccion_de_archivo = obtener_direccion_de_archivo nombre_de_archivo
+  	  "El resultado de la factorizacion ha sido escrito con exito en " + direccion_de_archivo
     end
-
   end
 
 end
--- factores-primos-3/spec/escritor_de_archivos_spec.rb
+++ factores-primos-3/spec/escritor_de_archivos_spec.rb
@@ -13,15 +13,24 @@ describe EscritorDeArchivo do
     expect(escritor.salida).to eq "salida"
   end  
 
+  it 'deberia indicar la direccion completa del archivo a escribir' do  	
+    escritor = EscritorDeArchivo.new "salida" , "--output-file=archivo.txt"
+    direccion_absoluta_de_archivo = File.dirname(__FILE__)
+    direccion_de_archivo = Pathname.new(direccion_absoluta_de_archivo).parent.to_s + "/archivo.txt"
+    expect(escritor.obtener_direccion_de_archivo "archivo.txt").to eq direccion_de_archivo
+  end 
+
   it 'deberia indicar que el archivo ha sido escrito con exito' do  	
     escritor = EscritorDeArchivo.new "salida" , "--output-file=archivo.txt"
-    salida = "El resultado de la factorizacion ha sido escrito con exito en archivo.txt"
+    direccion_absoluta_de_archivo = File.dirname(__FILE__)
+    direccion_de_carpeta_madre = Pathname.new(direccion_absoluta_de_archivo).parent.to_s
+    salida = "El resultado de la factorizacion ha sido escrito con exito en " + direccion_de_carpeta_madre + "/archivo.txt"
     expect(escritor.escribir_archivo).to eq salida
   end  
 
   it 'deberia indicar que no se ha especificado un nombre de archivo' do  	
     escritor = EscritorDeArchivo.new "salida" , "--output-file="
-    salida = "No ha especificado un nombre de archivo. Por favor, intente nuevamente."
+    salida = "No ha especificado el nombre del archivo a escribir. Por favor, intente nuevamente."
     expect(escritor.escribir_archivo).to eq salida
   end 
 
